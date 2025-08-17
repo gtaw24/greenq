20cca4bfd6d6ffa5c4cbbc19f29d6db3fddf3212
D3nga24 <131622601+gtaw24@users.noreply.github.com>
2025-08-17 21:18:03 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_KEY';
+const supabaseUrl = 'https://ollowdkvwdrnigzuritv.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sbG93ZGt2d2RybmlnenVyaXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NTYzNTAsImV4cCI6MjA3MTAzMjM1MH0.JL5EIR3Zssukoj0RP3kVeHRDrhLwvqJGfjZa--6ExR8';
 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
